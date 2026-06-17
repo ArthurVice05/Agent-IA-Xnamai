b1f6d5eb71cf498fb8194fe372bbb869220df03d
Arthur Viceles <vicelesarthur@gmail.com>
2026-06-17 17:04:47 -0300

    Mudando dados do sistema

--- routes/api.py
+++ routes/api.py
@@ -3,25 +3,10 @@ from services.openai_service import perguntar_ia
 
 router = APIRouter()
 
-@router.get("/chat")
-def chat(mensagem: str):
-
-    resposta = perguntar_ia(mensagem)
-
-    return {
-        "pergunta": mensagem,
-        "resposta": resposta
-    }
-
 @router.post("/webhook")
 async def webhook(data: dict):
 
-    print("DADOS RECEBIDOS:", data)
+    print("WEBHOOK RECEBIDO:")
+    print(data)
 
-    mensagem = data.get("mensagem", "")
-
-    resposta = perguntar_ia(mensagem)
-
-    return {
-        "resposta": resposta
-    }
+    return {"status": "ok"}
