e1cdf80686dd28e04fb95120b2787103c160224b
Arthur Viceles <vicelesarthur@gmail.com>
2026-06-18 16:21:59 -0300

    Ajustes do sistema

--- services/zapi_service.py
+++ services/zapi_service.py
@@ -1,31 +1,21 @@
 import requests
 
-INSTANCE_ID = "SEU_INSTANCE_ID"
-TOKEN = "SEU_TOKEN"
+INSTANCE_ID = "3F4CB317B66E2245E7E58645B9B7D1FC"
+TOKEN = "4C2EDD29BAA28D8A82485C7B"
 
 def enviar_mensagem(numero, mensagem):
 
     url = f"https://api.z-api.io/instances/{INSTANCE_ID}/token/{TOKEN}/send-text"
 
     payload = {
-        "phone": str(numero),
-        "message": str(mensagem)
+        "phone": numero,
+        "message": mensagem
     }
 
-    try:
+    response = requests.post(
+        url,
+        json=payload
+    )
 
-        response = requests.post(
-            url,
-            json=payload,
-            timeout=30
-        )
-
-        print("STATUS ZAPI:", response.status_code)
-        print("RESPOSTA ZAPI:", response.text)
-
-        return response.text
-
-    except Exception as e:
-
-        print("ERRO ZAPI:", str(e))
-        return None
+    print("STATUS ZAPI:", response.status_code)
+    print("RESPOSTA ZAPI:", response.text)
